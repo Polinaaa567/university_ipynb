be79b031c42210b4ea51adda278152cc5e658eba
Kolesnik Polina <kolesnik.03.polina@gmail.com>
2023-03-26 18:22:45 +0700

    work16

--- БД/лаб 5/5.sql
+++ БД/лаб 5/5.sql
@@ -2,13 +2,16 @@
 select e.first_name, e.last_name, to_date(e.start_date)
 from s_emp e
 where e.dept_id = (select e1.dept_id 
-from s_emp e1 where e1.last_name = 'Magee');
-
+from s_emp e1 where e1.last_name = 'Magee')
+and e.last_name not like 'Magee';
 /*2*/
-select *
-from s_emp e;
+select e.id, e.first_name, e.last_name
+from s_emp e
+WHERE e.SALARY > (SELECT AVG(SALARY) FROM s_emp);
 /*3*/
-
+SELECT e.last_name, e. dept_id, e.title
+FROM S_EMP e 
+WHERE e.DEPT_ID IN (SELECT ID FROM S_DEPT WHERE REGION_ID IN (1,2));
 /*4*/
 select e.last_name, e.salary
 from s_emp e
@@ -16,11 +19,10 @@ where manager_id = (Select id from s_emp e1 where lower(e1.first_name) like 'lad
 and lower(e1.last_name) like 'ngao');
 
 /*5*/
-
 select e.id, e.first_name, e.last_name
 from s_emp e
 where e.salary >= (select avg(e1.salary)
-                    from s_emp e1)
+                    from s_emp e1
                     where lower(e1.last_name) like '%t%'
                     and e1.dept_id = e.dept_id);
 
@@ -36,9 +38,11 @@ or lower(e.last_name) like 'nguyen';
 select p.name, p.short_desc
 from s_ord o join s_item i on o.id = i.ord_id
 join s_product p on p.id = i.product_id
-where p.id not in (select i.product_id
-                    from s_ord o1 join s_item i1 on o1)
-
+where p.id not in (select i1.product_id
+                    from s_ord o1 join s_item i1 on o1.ID=i1.ORD_ID
+                    where TO_NUMBER(TO_CHAR(o1.DATE_ORDERED,'yyyy'))=1992
+                    AND TO_NUMBER(TO_CHAR(o1.DATE_ORDERED,'mm')) = 9);
+/*8*/
 
 
 
--- ЯП/лаб4/3.4.js
+++ ЯП/лаб4/3.4.js
@@ -1,13 +1,42 @@
 class Stock {
+    constructor() {
+        this.listW = [];
+        this.listV = [];
+    }
     add(w, v) {
-
+        this.listW.push(w);
+        this.listV.push(v);
     }
 
-    GetByW (min_w) {
-
-    }
-
-    GetByV(min_v) {
+    getByW(min_w) {
+        let setW = new Set();
+        for (let index = 0; index < this.listW.length; index++) {
+            if (this.listW[index] < min_w) {
+                setW.add(this.listW[index]);
+            }
+        }
+        let min = Math.min( ...setW );
+        return this.listW.indexOf(min); 
         
     }
+
+    getByV(min_v) {
+        let setV = new Set();
+        for (let index = 0; index < this.listV.length; index++) {
+            if (this.listV[index] < min_v) {
+                setV.add(this.listV[index]);
+            }
+        }
+        let min = Math.min( ...setV );
+        return this.listV.indexOf(min); 
+    }
+
 }
+
+
+let stock1 = new Stock();
+stock1.add(5, 7);
+stock1.add(4, 8);
+stock1.add(1, 8);
+console.log(stock1.getByV(2));
+console.log(stock1.getByW(6)); 
