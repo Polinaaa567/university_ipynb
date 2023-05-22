cb3c309f99f36bd028ad75b0b789fcbea06ba086
Kolesnik Polina <kolesnik.03.polina@gmail.com>
2023-05-22 23:15:11 +0700

    work56

--- Питон/лаб9/2.py
+++ Питон/лаб9/2.py
@@ -1,34 +1,29 @@
-import os
-import time
 import asyncio
-from datetime import datetime
-
-COEFF = 1
+import aiohttp
 
 
-async def dish(el):
-    print(f"{el[0]} started the 1 task.")
-    await asyncio.sleep(el[1] / 100)
-    print(f"{el[0]} moved on to the defense of the 1 task.")
-    await asyncio.sleep(el[2] / 100)
-    print(f"{el[0]} completed the 1 task.")
-    print(f"{el[0]} is resting.")
-    await asyncio.sleep(5 / 100)
-    print(f"{el[0]} started the 2 task.")
-    await asyncio.sleep(el[3] / 100)
-    print(f"{el[0]} moved on to the defense of the 2 task.")
-    await asyncio.sleep(el[4] / 100)
-    print(f"{el[0]} completed the 2 task.")
+async def get_ip(service):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(service) as response:
+                if response.status == 200:
+                    return await response.text(), service
+    except:
+        pass
 
 
-async def interviews(*data):
-    tasks = [
-        asyncio.create_task(dish(i)) for i in data
-    ]
-    await asyncio.gather(*tasks)
+async def main():
+    services = ['https://api.ipify.org', 'http://ip-api.com']
+    tasks = [get_ip(service) for service in services]
+    done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+    for task in done:
+        result = task.result()
+        if result:
+            ip, service = result
+            print(f"Your IP address is {ip} (from {service})")
+            break
 
 
 if __name__ == '__main__':
-    t0 = time.time()  # запоминаем время начала работы
-    if os.name == 'nt':
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
--- Питон/лаб9/7.py
+++ Питон/лаб9/7.py
@@ -9,48 +9,25 @@ config = {
 
 
 class HWBot(discord.Client):
-    def random_dog(self):
-        url1 = "https://dog.ceo/api/breeds/image/random"
-        res = requests.get(url1)
-        data = res.json()
-        if 'message' in data:
-            return data['message']
-        return None
-
-    def random_cat(self):
-        url2 = "https://api.thecatapi.com/v1/images/search"
-        res2 = requests.get(url2)
-        data2 = res2.json()
-        if 'url' in data2:
-            return data2['url']
-        return None
-
     async def on_ready(self):
         print(f'{self.user} Привет! Я подключился и готов показать котика или собачку')
 
     async def on_message(self, message):
-        random_dog_png = self.random_dog()
-        random_cat_png = self.random_cat()
-        print(random_cat_png)
-        print(random_dog_png)
-
         if message.author == self.user:
             return
         if "собака" in message.content.lower():
+            response = requests.get("https://dog.ceo/api/breeds/image/random").json()
+            dog_url = response['message']
             embed = discord.Embed(title="рандомная собака!", color=discord.Color.purple())
-            embed.set_image(url=random_dog_png)
+            embed.set_image(url=dog_url)
             await message.channel.send(embed=embed)
 
         elif "кот" in message.content.lower():
-            embed = discord.Embed(title="рандомный кот!", color=discord.Color.purple())
-            embed.set_image(url=random_cat_png)
-            await message.channel.send(embed=embed)
-            # with open('cat.jpg', 'rb') as f:
-            # embed = discord.Embed(title="Doggo!", color=discord.Color.purple())
-            # embed.set_image(url=catjson['url'])
-            # await message.send(embed=embed)
-            # picture = discord.File(f)
-            # await message.channel.send(file=picture)
+            response = requests.get("https://api.thecatapi.com/v1/images/search").json()
+            cat_url = response[0]['url']
+            random_cat_png = discord.Embed(title="рандомный кот!", color=discord.Color.purple())
+            random_cat_png.set_image(url=cat_url)
+            await message.channel.send(embed=random_cat_png)
 
 
 intents = discord.Intents.default()
