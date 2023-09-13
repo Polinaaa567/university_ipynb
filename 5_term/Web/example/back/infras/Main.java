90c81268fa97d36e98ac9f6972f080c468809fb3
Kolesnik Polina <kolesnik.03.polina@gmail.com>
2023-09-13 22:28:46 +0700

    Web Изменение примера

--- 5_term/Web/example/back/infras/Main.java
+++ 5_term/Web/example/back/infras/Main.java
@@ -5,9 +5,6 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
-// import app.*;
-// import dom.*;
-
 public class Main {
     public static void main(String[] args) throws IOException {
         MyHandler mh = new MyHandler();
@@ -17,12 +14,6 @@ public class Main {
         server.setExecutor(null);
         server.start();
         System.out.println("Сервер запущен на порту 90");
-
-        // Calc calc = new Calc();
-        // CalcService calcService = new CalcService(calc);
-
-        // int result = calcService.calcadd(2, 2);
-        // System.out.println(result);
     }
 
 }
--- 5_term/Web/example/back/infras/MyHandler.java
+++ 5_term/Web/example/back/infras/MyHandler.java
@@ -1,5 +1,8 @@
 package infras;
 
+import app.CalcService;
+import dom.Calc;
+
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpExchange;
 
@@ -7,11 +10,24 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 class MyHandler implements HttpHandler {
+    private CalcService calcService;
+
+    public MyHandler() {
+        Calc calc = new Calc();
+        this.calcService = new CalcService(calc);
+    }
+
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        String response = "Hello world!";
+        int a = 2;
+        int b = 2;
+
+        int result = calcService.calcadd(a, b);
+
+        String response = a + " + " + b + " = " + result;
         exchange.sendResponseHeaders(200, response.getBytes().length);
         OutputStream os = exchange.getResponseBody();
+        System.out.println("Отправилось значение 4");
         os.write(response.getBytes());
         os.close();
     }
